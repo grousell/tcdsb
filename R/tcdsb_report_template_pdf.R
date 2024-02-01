bb14435b4b20f245ed0b0bfd43adcc1d3a64ea32
Rousell <rouselg@tcdsb.org>
2024-02-01 14:50:34 -0500

    Add tcdsb_report_template function

---
+++ R/tcdsb_report_template_pdf.R
@@ -0,0 +1,49 @@
+#' Report Template - PDF
+#' A function that downloads an .Rmd template, images and .tex files for a TCDSB themed report template.
+#' @param report_name
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' # tcdsb_report_template_pdf(test_report)
+
+tcdsb_report_template_pdf <- function(report_name){
+
+  report_name <- deparse(substitute(report_name)) #makes names a character string
+
+  # Download .rmd file ----
+  download.file("https://raw.githubusercontent.com/grousell/tcdsb/master/templates/tcdsb_report_template.Rmd",
+                destfile = glue::glue("{report_name}.rmd")
+  )
+
+  # Download .tex template ----
+
+  download.file("https://raw.githubusercontent.com/grousell/tcdsb/master/templates/in-header.tex",
+                destfile = glue::glue("in-header.tex")
+  )
+
+  # Create images file if needed ----
+  if (file.exists("images")){
+
+  } else {
+    dir.create("images")
+  }
+
+  # Download TCDSB images ----
+
+  download.file("https://github.com/grousell/tcdsb/blob/master/images/r_a_footer.png",
+                destfile = "images/r_a_footer.png",
+                mode = "wb")
+
+  download.file("https://github.com/grousell/tcdsb/blob/master/images/tcdsb_logo_BW.png",
+                destfile = "images/tcdsb_logo_BW.png",
+                mode = "wb")
+
+  download.file("https://github.com/grousell/tcdsb/blob/master/images/title_page_background.png",
+                destfile = "images/tcdsb_logo_BW.png",
+                mode = "wb")
+
+}
+
+
