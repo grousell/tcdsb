47c4cdbc5d8cc009b1117e0ca427df9344cec422
Greg Rousell <grousell@gmail.com>
2024-11-08 08:44:22 -0500

    Minor updates

--- R/tcdsb_report_template_word.R
+++ R/tcdsb_report_template_word.R
@@ -1,4 +1,14 @@
 
+#' Report Template - Word
+#' A function that downloads an .Rmd template, .R script and Word templates for a TCDSB themed report template.
+#' @param report_name
+#' Name of the report to be created.
+#' @return
+#' Templates and scripts to create the report
+#' @export
+#'
+#' @examples
+
 tcdsb_report_template_word <- function(report_name){
   if(file.exists("assets")){
   } else{
@@ -22,9 +32,13 @@ tcdsb_report_template_word <- function(report_name){
 
   # Download report template ----
   download.file("https://github.com/grousell/tcdsb/blob/master/templates/tcdsb_word_template.docx",
-                destfile = glue::glue("assets/{report_name}.docx")
+                destfile = "assets/tcdsb_word_template.docx"
+  )
+
+  # Download .rmd ----
+  download.file("https://github.com/grousell/tcdsb/blob/master/templates/tcdsb_report_template_word.Rmd",
+                destfile = glue::glue("{report_name}.rmd")
   )
 }
 
 
-tcdsb_report_template_word()
--- templates/01_create_word_coverpage.R
+++ templates/01_create_word_coverpage.R
@@ -8,7 +8,7 @@ author2 <- "Second Author"
 
 # Set up Title Page -------------------------------------------------------
 
-officer::read_docx("templates/tcdsb_word_cover_page_template.docx") |>
+officer::read_docx("assets/tcdsb_word_cover_page_template.docx") |>
   officer::body_replace_all_text(
     old_value = "TITLE",
     new_value = title
