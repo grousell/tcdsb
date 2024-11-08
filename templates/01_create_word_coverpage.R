528775a01588deab21bc1df2d2fdf9c76c6d3970
Greg Rousell <grousell@gmail.com>
2024-11-08 08:18:05 -0500

    Add Word templates to repo

---
+++ templates/01_create_word_coverpage.R
@@ -0,0 +1,33 @@
+# Set up ------------------------------------------------------------------
+
+title <- "My New Title"
+subtitle <- "My New Subtitle"
+date <- as.character(Sys.Date())
+author <- "Person who Authored the Report"
+author2 <- "Second Author"
+
+# Set up Title Page -------------------------------------------------------
+
+officer::read_docx("templates/tcdsb_word_cover_page_template.docx") |>
+  officer::body_replace_all_text(
+    old_value = "TITLE",
+    new_value = title
+  ) |>
+  officer::body_replace_all_text(
+    old_value = "SUB",
+    new_value = subtitle,
+  ) |>
+  officer::body_replace_all_text(
+    old_value = "DATE",
+    new_value = date
+  ) |>
+  officer::body_replace_all_text(
+    old_value = "AUTHOR",
+    new_value = author
+  ) |>
+  officer::body_replace_all_text(
+    old_value = "AUTH2",
+    new_value = author2
+  ) |>
+  print(target = "title_page.docx")
+
