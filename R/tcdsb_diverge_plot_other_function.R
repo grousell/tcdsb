84a3ece4d0bd5fafea34d45fb920e6b3305ed1d0
Greg Rousell <grousell@gmail.com>
2025-12-03 09:23:02 -0500

    Change label font size.

--- R/tcdsb_diverge_plot_other_function.R
+++ R/tcdsb_diverge_plot_other_function.R
@@ -78,12 +78,12 @@ tcdsb_diverge_plot_other_function <- function(df,
                   label = scales::percent({{ percent_col }}, accuracy = 1),
                   hjust = -1
     ),
-    size = 4 * font_size,
+    size = 3 * font_size,
     family = font) +
     # Add TITLE
     ggplot2::annotate("text", x = (x_max / 2), y = num_groups + 0.75,
              label = label,
-             size = 4 * font_size,                # Font size (in mm, not pt)
+             size = 3 * font_size,                # Font size (in mm, not pt)
              family = font,
              fontface = 'bold',) +
     ggplot2::labs(x = NULL, y = NULL) +
