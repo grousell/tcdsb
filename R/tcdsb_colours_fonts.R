5a1671230098c6182b1346d580c790fe9e7986f4
Greg Rousell <grousell@gmail.com>
2026-04-09 10:51:18 -0400

    Updated tcdsb-colors_fonts() function and documentation.

--- R/tcdsb_colours_fonts.R
+++ R/tcdsb_colours_fonts.R
@@ -2,27 +2,30 @@
 #' TCDSB Colors and Fonts
 #'
 #' @param ...
-#' Leave blank.
+#' Not used. Included for possible future use.
 #' @return
 #' R objects with names and HEX colors for TCDSB visual identity
 #' @export
 #'
 #' @examples
-#' # tcdsb_colours_fonts()
+#' # theme <- tcdsb_colours_fonts()
+#' # theme$tcdsb_board_color
+#' # "#951B1E"
 
-tcdsb_colours_fonts <- function(...){
 
-  assign("tcdsb_board_color", "#951B1E", envir = .GlobalEnv)
-  assign("tcdsb_board_color_20", "#C84E51", envir = .GlobalEnv)
-  assign("tcdsb_board_color_40", "#FB8184", envir = .GlobalEnv)
+tcdsb_colours_fonts <- function(...) {
+  list(
+    tcdsb_board_color = "#951B1E",
+    tcdsb_board_color_20 = "#C84E51",
+    tcdsb_board_color_40 = "#FB8184",
 
-  assign("tcdsb_palette", c("#560F11", "#2D0026", "#BA7D6B", "#FFE3A6", "#8EB8C2",
-                            "#6BCAD4", "#016567", "#7DA387", "#8ACA88", "#9D976E"),
-         envir = .GlobalEnv)
+    tcdsb_palette = c(
+      "#560F11", "#2D0026", "#BA7D6B", "#FFE3A6", "#8EB8C2",
+      "#6BCAD4", "#016567", "#7DA387", "#8ACA88", "#9D976E"
+    ),
 
-  assign("province_green", "#39B54A", , envir = .GlobalEnv)
-
-  assign("tcdsb_font", "Century Gothic", envir = .GlobalEnv)
-  }
-
+    province_green = "#39B54A",
+    tcdsb_font = "Century Gothic"
+  )
+}
 
--- R/tcdsb_presentation_template_quarto.R
+++ R/tcdsb_presentation_template_quarto.R
@@ -1,17 +1,7 @@
-#' Report Template - Quarto PDF
-#' A function that downloads an .qmd template and associate .typ and .lua files for a TCDSB themed report template.
-#' @param report_name
-#' Name of the report to be created.
-#' @return
-#' Templates and scripts to create the report
-#' @export
-#'
-#' @examples
-#' #tcdsb_report_template_quarto("test_report")
 
 tcdsb_report_template_quarto <- function(report_name){
+
   report_name <- deparse(substitute(report_name)) #makes names a character string
-  # report_name <- "report_template"
 
   if(file.exists("assets")){
   } else{
@@ -23,42 +13,43 @@ tcdsb_report_template_quarto <- function(report_name){
     dir.create("R")
   }
 
+  if(file.exists("styles")){
+  } else{
+    dir.create("styles")
+  }
+
   # Download .qmd ----
 
-  download.file("https://raw.githubusercontent.com/grousell/tcdsb/master/templates/tcdsb_report_template.qmd",
+  download.file("https://raw.githubusercontent.com/grousell/tcdsb/master/templates/tcdsb_presentation_template_revealjs.qmd",
                 destfile = glue::glue("{report_name}.qmd")
   )
 
-  # Download .typ files ----
-  download.file("https://raw.githubusercontent.com/grousell/tcdsb/refs/heads/master/templates/typst-show.typ",
-                destfile = "typst-show.typ"
-                )
-
-  download.file("https://raw.githubusercontent.com/grousell/tcdsb/refs/heads/master/templates/typst-template.typ",
-                destfile = "typst-template.typ"
+  # Download clean title page html file ----
+  download.file("https://raw.githubusercontent.com/grousell/tcdsb/refs/heads/master/templates/clean_title_page.html",
+                destfile = "styles/clean_title_page.html"
   )
 
-  # Download .lua files ----
-  download.file("https://raw.githubusercontent.com/grousell/tcdsb/refs/heads/master/templates/colbreak.lua",
-                destfile = "colbreak.lua"
+  # Download .css and .scss files ----
+
+  download.file("https://raw.githubusercontent.com/grousell/tcdsb/refs/heads/master/templates/tcdsb_revealjs.scss",
+                destfile = "styles/tcdsb_revealjs.scss"
+  )
+
+  download.file("https://raw.githubusercontent.com/grousell/tcdsb/refs/heads/master/templates/tcdsb_extras.css",
+                destfile = "styles/tcdsb_extras.css"
   )
 
 
   # Download TCDSB images ----
-  download.file("https://github.com/grousell/tcdsb/blob/master/images/title_page_background.png?raw=true",
-                destfile = "assets/title_page_background.png",
-                mode = "wb")
 
-  download.file("https://github.com/grousell/tcdsb/blob/master/images/tcdsb_logo_maroon.png?raw=true",
-                destfile = "assets/tcdsb_logo_maroon.png",
+  download.file("https://github.com/grousell/tcdsb/blob/master/assets/title_slide_background.png?raw=true",
+                destfile = "assets/title_slide_background.png",
                 mode = "wb")
 
   download.file("https://github.com/grousell/tcdsb/blob/master/images/r_a_logo.png?raw=true",
                 destfile = "assets/r_a_logo.png",
                 mode = "wb")
 
-  }
-
-tcdsb_report_template_quarto()
+}
 
 
